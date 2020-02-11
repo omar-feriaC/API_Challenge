0d04b7db01c8c843d5607768aae1598d3c0da2f9
dluna86 <47647769+dluna86@users.noreply.github.com>
2020-02-10 18:17:37 -0600

    more questions

--- challenge-master/BaseFramework/REST.cs
+++ challenge-master/BaseFramework/REST.cs
@@ -69,6 +69,7 @@ namespace BaseFramework.Rest
                 if (!String.IsNullOrEmpty(body))
                 {
                     //We should probably add our body to the request's content here
+                    
                 }
             }
 
