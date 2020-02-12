bfe340b94172ffd1b3f6be2f61704796b6c63a03
dluna86 <47647769+dluna86@users.noreply.github.com>
2020-02-12 11:59:57 -0600

    Clear method setup

--- challenge-master/BaseFramework/WebPages/MainPage.cs
+++ challenge-master/BaseFramework/WebPages/MainPage.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
 
 namespace BaseFramework.WebPages
 {
@@ -13,8 +15,8 @@ namespace BaseFramework.WebPages
     {
         /*ATTRIBUTES*/
         public static readonly string url = "http://ztestqa.com/selenium/mainpage.html";
-        private IWebDriver driver;
-        public static WebDriverWait _driverWait;
+        //private IWebDriver _pobjdriver;
+        public static WebDriverWait _driverWait; //= new WebDriverWait(_objDriver, new TimeSpan(0, 0, 40));
         private static IWebDriver _objDriver;
 
         /*LOCATORS DESCRIPTION*/
@@ -26,23 +28,76 @@ namespace BaseFramework.WebPages
         readonly static string STR_CHKBX_PLUS = "//input[@id='EZSMF_CBX3']";
         readonly static string STR_CHKBX_P = "//input[@id='VEX8W_CBX4']";
         readonly static string STR_DD_SEL = "//div[@name='selOpt']/select";
-        readonly static string STR_DD_SEL_OPT_1 = "//div[@name='selOpt']/select/option[text()='1']";
-        readonly static string STR_DD_SEL_OPT_2 = "//div[@name='selOpt']/select/option[text()='2']";
-        readonly static string STR_DD_SEL_OPT_3 = "//div[@name='selOpt']/select/option[text()='3']";
-        readonly static string STR_DD_SEL_OPT_4 = "//div[@name='selOpt']/select/option[text()='4']";
-        readonly static string STR_DD_SEL_OPT_5 = "//div[@name='selOpt']/select/option[text()='5']";
-        readonly static string STR_DD_SEL_OPT_6 = "//div[@name='selOpt']/select/option[text()='6']";
+        readonly static string STR_DD_SEL_OPT_1 = "//div[@name='selOpt']/select/option[@value='SEL_DX3GY']";
+        readonly static string STR_DD_SEL_OPT_2 = "//div[@name='selOpt']/select/option[@value='SEL_AGSRO']";
+        readonly static string STR_DD_SEL_OPT_3 = "//div[@name='selOpt']/select/option[@value='SEL_EZSMF']";
+        readonly static string STR_DD_SEL_OPT_4 = "//div[@name='selOpt']/select/option[@value='SEL_VEX8W']";
+        readonly static string STR_DD_SEL_OPT_5 = "//div[@name='selOpt']/select/option[@value='SEL_KSLUH']";
+        readonly static string STR_DD_SEL_OPT_6 = "//div[@name='selOpt']/select/option[@value='SEL_4U3QU']";
         readonly static string STR_DD_NOSEL = "//div[@name='noSel']/select";
+        readonly static string STR_DD_NOSEL_OPT_0 = "//div[@name='noSel']/select/option[@value='empty']";
+        readonly static string STR_DD_NOSEL_OPT_1 = "//div[@name='noSel']/select/option[@value='SEL_1']";
+        readonly static string STR_DD_NOSEL_OPT_2 = "//div[@name='noSel']/select/option[@value='SEL_2']";
+        readonly static string STR_DD_NOSEL_OPT_3 = "//div[@name='noSel']/select/option[@value='SEL_3']";
+        readonly static string STR_DD_NOSEL_OPT_4 = "//div[@name='noSel']/select/option[@value='SEL_4']";
+        readonly static string STR_DD_NOSEL_OPT_5 = "//div[@name='noSel']/select/option[@value='SEL_5']";
+        readonly static string STR_DD_NOSEL_OPT_6 = "//div[@name='noSel']/select/option[@value='SEL_6']";
         readonly static string STR_BTN_SUBMIT = "//button[text()='Submit']";
 
         /*CONSTRUCTOR*/
-        public MainPage(IWebDriver driver)
+        public MainPage(IWebDriver pobjdriver)
         {
-            this.driver = driver;
+            _objDriver = pobjdriver;
             _driverWait = new WebDriverWait(_objDriver, new TimeSpan(0, 0, 40));
         }
 
-        SelectElement oSelect = new SelectElement(_objDriver.FindElement(By.Id("selectFilterbyUser")));
-        
+        /*OBJECT DEFINITION*/
+        private static IWebElement objFName => _objDriver.FindElement(By.XPath(STR_FNAME_INPUT));
+        private static IWebElement objLName => _objDriver.FindElement(By.XPath(STR_LNAME_INPUT));
+        private static IWebElement objCBXB => _objDriver.FindElement(By.XPath(STR_CHKBX_B));
+        private static IWebElement objCBXC => _objDriver.FindElement(By.XPath(STR_CHKBX_C));
+        private static IWebElement objCBXPLUS => _objDriver.FindElement(By.XPath(STR_CHKBX_PLUS));
+        private static IWebElement objCBXP => _objDriver.FindElement(By.XPath(STR_CHKBX_P));
+        private static IWebElement objDDSel => _objDriver.FindElement(By.XPath(STR_DD_SEL));
+        private static IWebElement objDDSel1 => _objDriver.FindElement(By.XPath(STR_DD_SEL_OPT_1));
+        private static IWebElement objDDSel2 => _objDriver.FindElement(By.XPath(STR_DD_SEL_OPT_2));
+        private static IWebElement objDDSel3 => _objDriver.FindElement(By.XPath(STR_DD_SEL_OPT_3));
+        private static IWebElement objDDSel4 => _objDriver.FindElement(By.XPath(STR_DD_SEL_OPT_4));
+        private static IWebElement objDDSel5 => _objDriver.FindElement(By.XPath(STR_DD_SEL_OPT_5));
+        private static IWebElement objDDSel6 => _objDriver.FindElement(By.XPath(STR_DD_SEL_OPT_6));
+        private static IWebElement objDDNoSel => _objDriver.FindElement(By.XPath(STR_DD_NOSEL));
+        private static IWebElement objDDNoSel0 => _objDriver.FindElement(By.XPath(STR_DD_NOSEL_OPT_0));
+        private static IWebElement objDDNoSel1 => _objDriver.FindElement(By.XPath(STR_DD_NOSEL_OPT_1));
+        private static IWebElement objDDNoSel2 => _objDriver.FindElement(By.XPath(STR_DD_NOSEL_OPT_2));
+        private static IWebElement objDDNoSel3 => _objDriver.FindElement(By.XPath(STR_DD_NOSEL_OPT_3));
+        private static IWebElement objDDNoSel4 => _objDriver.FindElement(By.XPath(STR_DD_NOSEL_OPT_4));
+        private static IWebElement objDDNoSel5 => _objDriver.FindElement(By.XPath(STR_DD_NOSEL_OPT_5));
+        private static IWebElement objDDNoSel6 => _objDriver.FindElement(By.XPath(STR_DD_NOSEL_OPT_6));
+        private static IWebElement objBtnSubmit => _objDriver.FindElement(By.XPath(STR_BTN_SUBMIT));
+
+        /*METHODS/FUNCTIONS*/
+
+        private IWebElement GetClearElements()
+        {
+            return objFName;
+            
+        }
+
+        public static void fnClearAll()
+        {
+            _driverWait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.XPath(STR_FNAME_INPUT)));
+            objFName.Clear();
+            objLName.Clear();
+            if (objCBXB.Selected) objCBXB.Click();
+            if (objCBXC.Selected) objCBXC.Click();
+            if (objCBXPLUS.Selected) objCBXPLUS.Click();
+            if (objCBXP.Selected) objCBXP.Click();
+
+        }
+
+
+
+
+
     }
 }
--- challenge-master/Challenge.Test/Web_Tests.cs
+++ challenge-master/Challenge.Test/Web_Tests.cs
@@ -1,14 +1,23 @@
 ﻿using System;
+using BaseFramework.WebPages;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
 
 namespace Challenge.Test
 {
     [TestClass]
-    public class Web_Tests
+    public class Web_Tests:BaseTest
     {
-        [TestMethod]
+        //MainPage objMainPage = new MainPage(driver);
+        //IWebDriver driver ;
+
+        [Test]
         public void MainPage_Correct_Selections_Positive()
         {
+            MainPage objMainPage = new MainPage(driver);
+            MainPage.fnClearAll();
+            
         }
     }
 }
