1dbb61fbd27d7d61bb86535ef8e8de3e2a8b5b45
dluna86 <47647769+dluna86@users.noreply.github.com>
2020-02-13 12:06:08 -0600

    Final update

--- challenge-master/Challenge.Test/Web_Tests.cs
+++ challenge-master/Challenge.Test/Web_Tests.cs
@@ -12,80 +12,127 @@ namespace Challenge.Test
         [Test]
         public void MainPage_Correct_Selections_Positive()
         {
-            MainPage objMainPage = new MainPage(driver);
-            MainPage.fnClearAll();
-            MainPage.fnEnterFName("Daniel");
-            MainPage.fnEnterLName("Luna");
-            MainPage.fnCheckboxes(true, false, true, true);
-            MainPage.fnDDSelection(5);
-            MainPage.fnClickSubmit();
-            NUnit.Framework.Assert.AreEqual(MainPage.fnAlertMessages(0), MainPage.fnAlert()); 
+            try
+            {
+                MainPage objMainPage = new MainPage(driver);
+                MainPage.fnClearAll();
+                MainPage.fnEnterFName("Daniel");
+                MainPage.fnEnterLName("Luna");
+                MainPage.fnCheckboxes(true, false, true, true);
+                MainPage.fnDDSelection(5);
+                MainPage.fnClickSubmit();
+                NUnit.Framework.Assert.AreEqual(MainPage.fnAlertMessages(0), MainPage.fnAlert());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
 
         [Test]
         public void MainPage_Negative_FirstName_Empty()
         {
-            MainPage objMainPage = new MainPage(driver);
-            MainPage.fnClearAll();
-            MainPage.fnEnterFName("");
-            MainPage.fnEnterLName("Luna");
-            MainPage.fnCheckboxes(true, false, true, true);
-            MainPage.fnDDSelection(5);
-            MainPage.fnClickSubmit();
-            NUnit.Framework.Assert.AreEqual(MainPage.fnAlertMessages(1), MainPage.fnAlert());
+            try
+            {
+                MainPage objMainPage = new MainPage(driver);
+                MainPage.fnClearAll();
+                MainPage.fnEnterFName("");
+                MainPage.fnEnterLName("Luna");
+                MainPage.fnCheckboxes(true, false, true, true);
+                MainPage.fnDDSelection(5);
+                MainPage.fnClickSubmit();
+                NUnit.Framework.Assert.AreEqual(MainPage.fnAlertMessages(1), MainPage.fnAlert());
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
 
         [Test]
         public void MainPage_Negative_LastName_Empty()
         {
-            MainPage objMainPage = new MainPage(driver);
-            MainPage.fnClearAll();
-            MainPage.fnEnterFName("Daniel");
-            MainPage.fnEnterLName("");
-            MainPage.fnCheckboxes(true, false, true, true);
-            MainPage.fnDDSelection(5);
-            MainPage.fnClickSubmit();
-            NUnit.Framework.Assert.AreEqual(MainPage.fnAlertMessages(2), MainPage.fnAlert());
+            try
+            {
+                MainPage objMainPage = new MainPage(driver);
+                MainPage.fnClearAll();
+                MainPage.fnEnterFName("Daniel");
+                MainPage.fnEnterLName("");
+                MainPage.fnCheckboxes(true, false, true, true);
+                MainPage.fnDDSelection(5);
+                MainPage.fnClickSubmit();
+                NUnit.Framework.Assert.AreEqual(MainPage.fnAlertMessages(2), MainPage.fnAlert());
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
 
         [Test]
         public void MainPage_Negative_WrongCheckbox_Selected()
         {
-            MainPage objMainPage = new MainPage(driver);
-            MainPage.fnClearAll();
-            MainPage.fnEnterFName("Daniel");
-            MainPage.fnEnterLName("Luna");
-            MainPage.fnCheckboxes(true, true, true, true);
-            MainPage.fnDDSelection(5);
-            MainPage.fnClickSubmit();
-            NUnit.Framework.Assert.AreEqual(MainPage.fnAlertMessages(3), MainPage.fnAlert());
+            try
+            {
+                MainPage objMainPage = new MainPage(driver);
+                MainPage.fnClearAll();
+                MainPage.fnEnterFName("Daniel");
+                MainPage.fnEnterLName("Luna");
+                MainPage.fnCheckboxes(true, true, true, true);
+                MainPage.fnDDSelection(5);
+                MainPage.fnClickSubmit();
+                NUnit.Framework.Assert.AreEqual(MainPage.fnAlertMessages(3), MainPage.fnAlert());
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
 
         [Test]
         public void MainPage_Negative_WrongFirstDropdown_Selected()
         {
-            MainPage objMainPage = new MainPage(driver);
-            MainPage.fnClearAll();
-            MainPage.fnEnterFName("Daniel");
-            MainPage.fnEnterLName("Luna");
-            MainPage.fnCheckboxes(true, false, true, true);
-            MainPage.fnDDSelection(4);
-            MainPage.fnClickSubmit();
-            NUnit.Framework.Assert.AreEqual(MainPage.fnAlertMessages(4), MainPage.fnAlert());
+            try
+            {
+                MainPage objMainPage = new MainPage(driver);
+                MainPage.fnClearAll();
+                MainPage.fnEnterFName("Daniel");
+                MainPage.fnEnterLName("Luna");
+                MainPage.fnCheckboxes(true, false, true, true);
+                MainPage.fnDDSelection(4);
+                MainPage.fnClickSubmit();
+                NUnit.Framework.Assert.AreEqual(MainPage.fnAlertMessages(4), MainPage.fnAlert());
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
 
         [Test]
         public void MainPage_Negative_WrongSecondDropdown_Selected()
         {
-            MainPage objMainPage = new MainPage(driver);
-            MainPage.fnClearAll();
-            MainPage.fnEnterFName("Daniel");
-            MainPage.fnEnterLName("Luna");
-            MainPage.fnCheckboxes(true, false, true, true);
-            MainPage.fnDDSelection(5);
-            MainPage.fnDDNoSelection(5);
-            MainPage.fnClickSubmit();
-            NUnit.Framework.Assert.AreEqual(MainPage.fnAlertMessages(5), MainPage.fnAlert());
+            try
+            {
+                MainPage objMainPage = new MainPage(driver);
+                MainPage.fnClearAll();
+                MainPage.fnEnterFName("Daniel");
+                MainPage.fnEnterLName("Luna");
+                MainPage.fnCheckboxes(true, false, true, true);
+                MainPage.fnDDSelection(5);
+                MainPage.fnDDNoSelection(5);
+                MainPage.fnClickSubmit();
+                NUnit.Framework.Assert.AreEqual(MainPage.fnAlertMessages(5), MainPage.fnAlert());
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
 
 
