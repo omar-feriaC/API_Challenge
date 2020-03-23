bc63035237a9a6d458363a2b8c5df237b93c72dc
vidalvader <47647740+vidalvader@users.noreply.github.com>
2020-03-22 22:13:34 -0600

    AddingMyNotes

--- challenge-master/BaseFramework/REST.cs
+++ challenge-master/BaseFramework/REST.cs
@@ -6,75 +6,70 @@ using System.Net;
 using System.Net.Http;
 using System.IO;
 using System.Diagnostics;
-
 namespace BaseFramework.Rest
 {
-    public class Rest
+    public class clsRest
     {
-        private String baseUrl;
+        private String strbaseUrl;
         private Dictionary<String,String> headers;
-
         #region REST Constructor
-        public Rest(String url)
+        public clsRest(String pstrurl)
         {
-            this.baseUrl = url;
+            this.strbaseUrl = pstrurl;
             headers = new Dictionary<String, String>();
         }
         #endregion
 
         #region Add/Clear Headers
-        public void ClearHeaders()
+        public void fnClearHeaders()
         {
             headers.Clear();
         }
-        public bool AddHeader(String key, String value)
+        public bool fnAddHeader(String pstrkey, String pstrvalue)
         {
-            try { headers.Add(key, value); return true; }
+            try { headers.Add(pstrkey, pstrvalue); return true; }
             catch (Exception ex){ Debug.WriteLine(ex.Message);  return false; }
         }
         #endregion
-
-        #region GET Request
-        public HTTP_RESPONSE GET(String endpoint)
+        #region GET - Response of a GET
+        // needs the endpoint to know that to get
+        public clsHTTP_RESPONSE fnGET(String pstrEndpoint)
         {
-            return request("GET", endpoint);
+            return request("GET", pstrEndpoint);
+        }
+        #endregion
+        #region POST - Response of a POST
+        // needs the endpoint and the possible body to do a post
+        public clsHTTP_RESPONSE fnPOST(String pstrEndpoint, String pstrData)
+        {
+            return request("POST", pstrEndpoint, pstrData);
         }
         #endregion
 
-        #region POST Request
-        public HTTP_RESPONSE POST(String endpoint, String data)
+        #region private HTTP Request Generator
+        private clsHTTP_RESPONSE request(String requestType, String endpoint, String body = null)
         {
-            return request("POST", endpoint, data);
-        }
-        #endregion
-
-        #region HTTP Request Generator
-        private HTTP_RESPONSE request(String requestType, String endpoint, String body = null)
-        {
-
-            HttpWebRequest request = WebRequest.CreateHttp(baseUrl + endpoint);
-            HTTP_RESPONSE response = new HTTP_RESPONSE();
+            HttpWebRequest request = WebRequest.CreateHttp(strbaseUrl + endpoint);
+            clsHTTP_RESPONSE response = new clsHTTP_RESPONSE();
             Stopwatch responseTimer = new Stopwatch();
-
-            byte[] data = null;
-            request.Method = requestType;
+            //byte[] data = null;
+            request.Method = requestType; //This is where POST or GET is specified
             request.ContentType = "application/json";
             request.KeepAlive = false;
-
             foreach (KeyValuePair<String, String> kvp in headers)
                 request.Headers.Add(kvp.Key, kvp.Value);
-
-            if (!String.IsNullOrEmpty(body))
+            if (!String.IsNullOrEmpty(body))// i.e. if Body is abc, this will be false
             {
-               //We should probably add our body to the request's content here
+                //We should probably add our body to the request's content here
+                DataWriter = new StreamWriter(DataStream);
+                DataWriter.Write(body);
+                DataWriter.Flush();
             }
-
             responseTimer.Start();
             try
             {
                 using (HttpWebResponse webResponse = (HttpWebResponse)request.GetResponse())
-                    response = getResponseDetails(webResponse);
-
+                response = getResponseDetails(webResponse);
                 response.Time = responseTimer.Elapsed;
             }
             catch (WebException exception)
@@ -82,43 +77,40 @@ namespace BaseFramework.Rest
                 if (exception.Status == WebExceptionStatus.ProtocolError)
                 {
                     using (HttpWebResponse errResponse = (HttpWebResponse)exception.Response)
-                        response = getResponseDetails(errResponse);
+                    response = getResponseDetails(errResponse);
                     response.Time = responseTimer.Elapsed;
                 }
                 else throw new Exception(exception.Message);
             }
             responseTimer.Stop();
-
             return response;
         }
-
-        private HTTP_RESPONSE getResponseDetails(HttpWebResponse webResponse)
+        private clsHTTP_RESPONSE getResponseDetails(HttpWebResponse webResponse)
         {
-            HTTP_RESPONSE output = new HTTP_RESPONSE();
+            clsHTTP_RESPONSE output = new clsHTTP_RESPONSE();
 
-            //We should probably pull the Http status code and message body out of the webresposne in here
+            //We should probably pull the Http status code and 
+            //message body out of the webresposne in here
             //and put it in the HTTP_RESPONSE object.
 
             return output;
         }
         #endregion
-
     }
-
-    #region Http Response Class
-    public class HTTP_RESPONSE
+    #region Http clsResponse Class
+    public class clsHTTP_RESPONSE
     {
         public HttpStatusCode StatusCode;
         public Dictionary<String,String> Headers;
-        public String MessageBody;
+        public String strMessageBody;
         public TimeSpan Time;
-
-        public HTTP_RESPONSE()
+        //Constructor, just doing an action       
+        public clsHTTP_RESPONSE()
         {
             Headers = new Dictionary<String, String>();
         }
-
-        public void PopulateHeaders(WebHeaderCollection headersIn)
+        //Function expecting a header to populate those
+        public void fnPopulateHeaders(WebHeaderCollection headersIn)
         {
             for (int i = 0; i < headersIn.Count; i++)
             {
@@ -127,5 +119,4 @@ namespace BaseFramework.Rest
         }
     }
     #endregion
-
 }
