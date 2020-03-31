e2fbf5f37076558660c4d9593918733732c926df
Jose Pasos <pasos07.jp@gmail.com>
2020-03-31 11:24:09 -0600

    3rd Commit

--- challenge-master/BaseFramework/WebPages/MainPage.cs
+++ challenge-master/BaseFramework/WebPages/MainPage.cs
@@ -5,17 +5,95 @@ using System.Text;
 using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace BaseFramework.WebPages
 {
     public class MainPage
     {
-        public static readonly string url = "http://ztestqa.com/selenium/mainpage.html";
-        private IWebDriver driver;
-
+        //Constructor
         public MainPage(IWebDriver driver)
         {
-            this.driver = driver;
+            this.objdriver = driver;
         }
+
+        private IWebDriver objdriver;
+
+        //Page Objects//
+        readonly static string STRFIRSTNAME = "_firstName";
+        readonly static string STRLASTNAME = "_lastName";
+        readonly static string STRBCHECKBOX = "chk1";
+        readonly static string STRCCHECKBOX = "chk2";
+        readonly static string STRPLUSCHECKBOX = "chk13";
+        readonly static string STRPCHECKBOX = "chk14";
+        readonly static string STRFIRSDROPDOWN = "//div[@name='selOpt']//select";
+        readonly static string STRSECONDDROPDOWN = "//div[@name='noSel']//select";
+        readonly static string STRSUBMITBUTTON = "//button[text()='Submit']";
+
+        private IWebElement objFirstName => objdriver.FindElement(By.Name(STRFIRSTNAME));
+        private IWebElement objLastName => objdriver.FindElement(By.Name(STRLASTNAME));
+        private IWebElement objBCheckBox => objdriver.FindElement(By.Name(STRBCHECKBOX));
+        private IWebElement objCChecjBox => objdriver.FindElement(By.Name(STRCCHECKBOX));
+        private IWebElement objPlusCheckBox => objdriver.FindElement(By.Name(STRPLUSCHECKBOX));
+        private IWebElement objPCheckBox => objdriver.FindElement(By.Name(STRPCHECKBOX));
+        private IWebElement objFirstDropDown => objdriver.FindElement(By.XPath(STRFIRSDROPDOWN));
+        private IWebElement objSecondDropDown => objdriver.FindElement(By.XPath(STRSECONDDROPDOWN));
+        private IWebElement objSubmitBtn => objdriver.FindElement(By.XPath(STRSUBMITBUTTON));
+
+        //Methods//
+        public void fnInputFirstName(string pstrFirstName)
+        {
+            objFirstName.Clear();
+            objFirstName.SendKeys(pstrFirstName);
+        }
+        public void fnInputLastName(string pstrLastName)
+        {
+            objLastName.Clear();
+            objLastName.SendKeys(pstrLastName);
+        }
+     
+        public void fnClickBCheckBox()
+        {
+            objBCheckBox.Click();
+        }
+        public void fnClickCCheckbox()
+        {
+            objCChecjBox.Click();
+        }
+        public void fnClickPlusCheckbox()
+        {
+            objPlusCheckBox.Click();
+        }
+        public void fnClickPCheckbox()
+        {
+            objPCheckBox.Click();
+        }
+        public void fnClickSubmitButton()
+        {
+            objSubmitBtn.Click();
+        }
+        public void fnSelectFirstDropDown(string pstrItem)
+        {
+            var selectElement = new SelectElement(objFirstDropDown);
+            selectElement.SelectByText(pstrItem);
+        }
+        public void fnSelectSecondDropDown(string pstrItem)
+        {
+            var selectElement = new SelectElement(objSecondDropDown);
+            selectElement.SelectByText(pstrItem);
+        }
+
+        public string ConfirmationWindow(string pstrMessage)
+        {
+            var window = objdriver.SwitchTo().Alert();
+            string messageinwindow = window.Text;
+            if (messageinwindow.Contains(pstrMessage))
+            {
+                window.Accept();
+            }
+            return messageinwindow;
+        }
+
     }
+
 }
--- challenge-master/Challenge.Test/Web_Tests.cs
+++ challenge-master/Challenge.Test/Web_Tests.cs
@@ -1,14 +1,161 @@
 ﻿using System;
+using BaseFramework.WebPages;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System.Configuration;
 
 namespace Challenge.Test
 {
     [TestClass]
+    
     public class Web_Tests
     {
+        public IWebDriver mdriver;
+        public MainPage objMainPage;
+        private static string strBrowser = ConfigurationManager.AppSettings.Get("ZetaUrl");
+        string pstrFname = "Jose";
+
+        [TestInitialize]
+        public void OpenBrowser()
+        {
+            mdriver = new ChromeDriver();
+            mdriver.Url = strBrowser;
+            mdriver.Manage().Window.Maximize();
+            objMainPage = new MainPage(mdriver);
+          
+        }
         [TestMethod]
         public void MainPage_Correct_Selections_Positive()
         {
+            string expectedSuccessMessage = "Congratulations " + pstrFname + "! Everything was properly populated.";
+            objMainPage.fnInputFirstName(pstrFname);
+            objMainPage.fnInputLastName("Pasos");
+            objMainPage.fnClickBCheckBox();
+            objMainPage.fnClickPlusCheckbox();
+            objMainPage.fnClickPCheckbox();
+            objMainPage.fnSelectFirstDropDown("5");
+            objMainPage.fnSelectSecondDropDown("");
+            objMainPage.fnClickSubmitButton();
+            string messagefrompage = objMainPage.ConfirmationWindow(expectedSuccessMessage);
+            Assert.AreEqual(expectedSuccessMessage, messagefrompage);
+        }
+
+        [TestMethod]
+        public void MainPage_Negative_FirstName_Empty()
+        {
+            //cleaning before test//
+            objMainPage.fnClickBCheckBox();
+            objMainPage.fnClickPlusCheckbox();
+            objMainPage.fnClickPCheckbox();
+
+            //Test//
+            string expectedErrorMessage1 = "Please enter a first name";
+            objMainPage.fnInputFirstName("");
+            objMainPage.fnInputLastName("Pasos");
+            objMainPage.fnClickBCheckBox();
+            objMainPage.fnClickPlusCheckbox();
+            objMainPage.fnClickPCheckbox();
+            objMainPage.fnSelectFirstDropDown("5");
+            objMainPage.fnSelectSecondDropDown("");
+            objMainPage.fnClickSubmitButton();
+            string messagefrompage = objMainPage.ConfirmationWindow(expectedErrorMessage1);
+            Assert.AreEqual(expectedErrorMessage1, messagefrompage);
+        }
+
+        [TestMethod]
+        public void MainPage_Negative_LastName_Empty()
+        {
+            //Cleaning before test//
+            objMainPage.fnClickBCheckBox();
+            objMainPage.fnClickPlusCheckbox();
+            objMainPage.fnClickPCheckbox();
+
+            //Test//
+            string expectedErrorMessage2 = "Please enter a last name";
+            objMainPage.fnInputFirstName(pstrFname);
+            objMainPage.fnClickBCheckBox();
+            objMainPage.fnClickPlusCheckbox();
+            objMainPage.fnClickPCheckbox();
+            objMainPage.fnSelectFirstDropDown("5");
+            objMainPage.fnSelectSecondDropDown("");
+            objMainPage.fnClickSubmitButton();
+            string messagefrompage = objMainPage.ConfirmationWindow(expectedErrorMessage2);
+            Assert.AreEqual(expectedErrorMessage2, messagefrompage);
+        }
+        [TestMethod]
+        public void MainPage_Negative_WrongCheckbox_Selected()
+        {
+            //Cleaning before test//
+            objMainPage.fnClickBCheckBox();
+            objMainPage.fnClickPlusCheckbox();
+            objMainPage.fnClickPCheckbox();
+
+            //Test//
+            string expectedErrorMessage3 = "The checkbox selection is not quite right";
+            objMainPage.fnInputFirstName(pstrFname);
+            objMainPage.fnInputFirstName("Pasos");
+            objMainPage.fnClickBCheckBox();
+            objMainPage.fnClickCCheckbox();
+            objMainPage.fnClickPlusCheckbox();
+            objMainPage.fnClickPCheckbox();
+            objMainPage.fnSelectFirstDropDown("5");
+            objMainPage.fnSelectSecondDropDown("");
+            objMainPage.fnClickSubmitButton();
+            string messagefrompage = objMainPage.ConfirmationWindow(expectedErrorMessage3);
+            Assert.AreEqual(expectedErrorMessage3, messagefrompage);
+        }
+
+        [TestMethod]
+        public void MainPage_Negative_WrongDropdown_SelectedinFirst()
+        {
+            //Cleaning Before Test//
+            objMainPage.fnClickBCheckBox();
+            objMainPage.fnClickCCheckbox();
+            objMainPage.fnClickPlusCheckbox();
+            objMainPage.fnClickPCheckbox();
+
+            //Test//
+            string expectedErrorMessage4 = "The dropdown selection is not quite right";
+            objMainPage.fnInputFirstName(pstrFname);
+            objMainPage.fnInputLastName("Pasos");
+            objMainPage.fnClickBCheckBox();
+            objMainPage.fnClickPlusCheckbox();
+            objMainPage.fnClickPCheckbox();
+            objMainPage.fnSelectFirstDropDown("4");
+            objMainPage.fnSelectSecondDropDown("");
+            objMainPage.fnClickSubmitButton();
+            string messagefrompage = objMainPage.ConfirmationWindow(expectedErrorMessage4);
+            Assert.AreEqual(expectedErrorMessage4, messagefrompage);
+        }
+
+        [TestMethod]
+        public void MainPage_Negative_WrongDropdown_SelectedinSecond()
+        {
+            //cleaning before test//
+            objMainPage.fnClickBCheckBox();
+            objMainPage.fnClickPlusCheckbox();
+            objMainPage.fnClickPCheckbox();
+
+            //Test//
+            string expectedErrorMessage5 = "A selection was made other than the default in select list 2";
+            objMainPage.fnInputFirstName(pstrFname);
+            objMainPage.fnInputLastName("Pasos");
+            objMainPage.fnClickBCheckBox();
+            objMainPage.fnClickPlusCheckbox();
+            objMainPage.fnClickPCheckbox();
+            objMainPage.fnSelectFirstDropDown("5");
+            objMainPage.fnSelectSecondDropDown("5");
+            objMainPage.fnClickSubmitButton();
+            string messagefrompage = objMainPage.ConfirmationWindow(expectedErrorMessage5);
+            Assert.AreEqual(expectedErrorMessage5, messagefrompage);
+        }
+
+        [TestCleanup]
+        public void Close()
+        {
+            mdriver.Close();
+            mdriver.Quit();
         }
     }
 }
