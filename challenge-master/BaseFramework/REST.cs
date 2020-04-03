130d1e6bd22f5c8f8dc56da729605d058d8d9d18
Aperez11 <alejandro.perez@4thsource.com>
2020-04-03 17:19:28 -0600

    API testing

--- challenge-master/BaseFramework/REST.cs
+++ challenge-master/BaseFramework/REST.cs
@@ -6,19 +6,23 @@ using System.Net;
 using System.Net.Http;
 using System.IO;
 using System.Diagnostics;
+using BaseFramework.Model;
 
 namespace BaseFramework.Rest
 {
     public class Rest
     {
-        private String baseUrl;
-        private Dictionary<String,String> headers;
+        #region Variabe
+        public String baseUrl;
+        public Dictionary<String, String> headers;
+        #endregion
 
         #region REST Constructor
         public Rest(String url)
         {
             this.baseUrl = url;
             headers = new Dictionary<String, String>();
+
         }
         #endregion
 
@@ -30,7 +34,7 @@ namespace BaseFramework.Rest
         public bool AddHeader(String key, String value)
         {
             try { headers.Add(key, value); return true; }
-            catch (Exception ex){ Debug.WriteLine(ex.Message);  return false; }
+            catch (Exception ex) { Debug.WriteLine(ex.Message); return false; }
         }
         #endregion
 
@@ -49,31 +53,48 @@ namespace BaseFramework.Rest
         #endregion
 
         #region HTTP Request Generator
-        private HTTP_RESPONSE request(String requestType, String endpoint, String body = null)
+        public HTTP_RESPONSE request(String requestType, String endpoint, String body = null)
         {
-
+            //Console.WriteLine(body);
             HttpWebRequest request = WebRequest.CreateHttp(baseUrl + endpoint);
+            //Console.WriteLine(baseUrl + endpoint);
             HTTP_RESPONSE response = new HTTP_RESPONSE();
             Stopwatch responseTimer = new Stopwatch();
+
 
             byte[] data = null;
             request.Method = requestType;
             request.ContentType = "application/json";
+            request.Accept = "application/json";
             request.KeepAlive = false;
 
             foreach (KeyValuePair<String, String> kvp in headers)
                 request.Headers.Add(kvp.Key, kvp.Value);
 
+
             if (!String.IsNullOrEmpty(body))
             {
-               //We should probably add our body to the request's content here
+                //We should probably add our body to the request's content here
+
+
+                data = ASCIIEncoding.ASCII.GetBytes(body);
+                request.ContentLength = data.Length;
+                Stream stream = request.GetRequestStream();
+
+                stream.Write(data, 0, data.Length);
+                stream.Close();
+
             }
 
             responseTimer.Start();
             try
             {
+                //HttpWebResponse webResponse = (HttpWebResponse)request.GetResponse();
                 using (HttpWebResponse webResponse = (HttpWebResponse)request.GetResponse())
                     response = getResponseDetails(webResponse);
+
+                //Console.WriteLine("CREATE POST RESPONSE IS: " + response.Headers);
+                //Console.WriteLine(response.MessageBody);
 
                 response.Time = responseTimer.Elapsed;
             }
@@ -92,12 +113,26 @@ namespace BaseFramework.Rest
             return response;
         }
 
-        private HTTP_RESPONSE getResponseDetails(HttpWebResponse webResponse)
+        public HTTP_RESPONSE getResponseDetails(HttpWebResponse webResponse)
         {
             HTTP_RESPONSE output = new HTTP_RESPONSE();
 
             //We should probably pull the Http status code and message body out of the webresposne in here
             //and put it in the HTTP_RESPONSE object.
+
+            //Console.WriteLine(((HttpWebResponse)webResponse).StatusDescription);
+            StreamReader reader;
+            using (var dataStream = webResponse.GetResponseStream())
+            {
+
+                reader = new StreamReader(dataStream);
+                string responseFromServer = reader.ReadToEnd();
+                reader.Close();
+                reader.Dispose();
+                output.StatusCode = webResponse.StatusCode;
+                output.MessageBody = responseFromServer;
+
+            }
 
             return output;
         }
@@ -109,7 +144,7 @@ namespace BaseFramework.Rest
     public class HTTP_RESPONSE
     {
         public HttpStatusCode StatusCode;
-        public Dictionary<String,String> Headers;
+        public Dictionary<String, String> Headers;
         public String MessageBody;
         public TimeSpan Time;
 
--- challenge-master/Challenge.Test/User_Tests.cs
+++ challenge-master/Challenge.Test/User_Tests.cs
@@ -5,41 +5,81 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Net;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using BaseFramework.Rest;
 using BaseFramework.Model;
+using Newtonsoft.Json;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
 
 namespace Challenge.Tests
 {
-    [TestClass]
     public class User_Tests
     {
         private readonly String baseUrl = ConfigurationManager.AppSettings["baseUrl"];
 
-        [TestMethod]
+        [Test]
         public void API_GET_Test()
         {
-            String endpoint = "/api/v1/employee/2"; 
-            Rest rest = new Rest(baseUrl);
-            HTTP_RESPONSE resp = rest.GET(endpoint);
+            String endpoint = "/api/v1/employees";
+
+            Rest restCall = new Rest(baseUrl);
+
+            HTTP_RESPONSE resp = restCall.GET(endpoint);
             Assert.AreEqual(HttpStatusCode.OK, resp.StatusCode, $"Expected Status Code {HttpStatusCode.OK}, Received {resp.StatusCode}");
             //We should probably do some more assertions here on the response to check that our GET request was successful.
+
+            EmployeeGet EmpGetDesSerialized = JsonConvert.DeserializeObject<EmployeeGet>(resp.MessageBody);
+            try
+            {
+                for (int i = 0; i < EmpGetDesSerialized.data.Count; i++)
+                {
+                    if (i == 1)
+                    {
+                        Console.WriteLine("Id: " + EmpGetDesSerialized.data.ElementAt(i).id);
+                        Console.WriteLine("Name: " + EmpGetDesSerialized.data.ElementAt(i).employee_name);
+                        Console.WriteLine("Salary: " + EmpGetDesSerialized.data.ElementAt(i).employee_salary);
+                        Console.WriteLine("Age: " + EmpGetDesSerialized.data.ElementAt(i).employee_age);
+                        Console.WriteLine("Profile Image: " + EmpGetDesSerialized.data.ElementAt(i).profile_image);
+                        Assert.AreEqual("2", EmpGetDesSerialized.data.ElementAt(i).id);
+                        Assert.AreEqual("Garrett Winters", EmpGetDesSerialized.data.ElementAt(i).employee_name);
+                        Assert.AreEqual("170750", EmpGetDesSerialized.data.ElementAt(i).employee_salary);
+                        Assert.AreEqual("63", EmpGetDesSerialized.data.ElementAt(i).employee_age);
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Assert.Fail();
+            }
         }
 
 
-        [TestMethod]
+        [Test]
         public void API_POST_Test()
         {
-            String endpoint = "/api/v1/create/";
-            User user = new User();
-            user.Name = "";
-            user.Salary = "";
-            user.Age = "";
-            Rest rest = new Rest(baseUrl);
-            HTTP_RESPONSE resp = rest.POST(endpoint, "");
-            //Need some assertions here to check the response.
+            try
+            {
+                String endpoint = "/api/v1/create";
+                User user = new User();
+                user.name = "Alejandro";
+                user.salary = "1";
+                user.age = "20";
+                Rest rest = new Rest(baseUrl);
+                String jsUser = JsonConvert.SerializeObject(user);
+                HTTP_RESPONSE resp = rest.POST(endpoint, jsUser);
+                Console.WriteLine(resp.MessageBody);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Assert.Fail();
+            }
+
         }
 
     }
-    
+
 }
