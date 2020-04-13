fa4e82ca3797ed46109e42c57d4bfcdf8bc107c2
Aperez11 <aperez@brierley.com>
2020-04-13 18:45:41 -0500

    Module 11 web pages

--- challenge-master/BaseFramework/WebPages/MainPage.cs
+++ challenge-master/BaseFramework/WebPages/MainPage.cs
@@ -5,17 +5,138 @@ using System.Text;
 using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
 
 namespace BaseFramework.WebPages
 {
     public class MainPage
     {
-        public static readonly string url = "http://ztestqa.com/selenium/mainpage.html";
-        private IWebDriver driver;
+        //  public static readonly string url = "http://ztestqa.com/selenium/mainpage.html";
+        private IWebDriver _driver;
+        private static readonly string FirstName = "_fNameInput";
+        private static readonly string LastName = "_lNameInput";
+        private static readonly string CheckBox1 = "DX3GY_CBX1";
+        private static readonly string CheckBox2 = "AGSRO_CBX2";
+        private static readonly string CheckBox3 = "EZSMF_CBX3";
+        private static readonly string CheckBox4 = "VEX8W_CBX4";
+        private static readonly string DropDownOption1 = "//div[@name='selOpt']//select";
+        private static readonly string DropDownOption2 = "//div[@name='noSel']//select";
+        private static readonly string SubmitButton = "//html[1]/body[1]/div[6]/button[1]";
+
+        public IWebElement objFirstName => _driver.FindElement(By.Id(FirstName));
+        public IWebElement objLastName => _driver.FindElement(By.Id(LastName));
+        public IWebElement objCheckBox1 => _driver.FindElement(By.Id(CheckBox1));
+        public IWebElement objCheckBox2 => _driver.FindElement(By.Id(CheckBox2));
+        public IWebElement objCheckBox3 => _driver.FindElement(By.Id(CheckBox3));
+        public IWebElement objCheckBox4 => _driver.FindElement(By.Id(CheckBox4));
+        public IWebElement objDropDownOption1 => _driver.FindElement(By.XPath(DropDownOption1));
+        public IWebElement objDropDownOption2 => _driver.FindElement(By.XPath(DropDownOption2));
+        public IWebElement objSubmitButton => _driver.FindElement(By.XPath(SubmitButton));
+
+        public string getFirstName()
+        {
+            return objFirstName.Text;
+        }
 
         public MainPage(IWebDriver driver)
         {
-            this.driver = driver;
+            this._driver = driver;
         }
+
+        public void clearFirstName()
+        {
+            objFirstName.Clear();
+        }
+
+        public void enterFirstName(string firstName)
+        {
+            objFirstName.SendKeys(firstName);
+        }
+
+        public void clearLastName()
+        {
+            objLastName.Clear();
+        }
+
+        public void enterLastName(string lastName)
+        {
+            objLastName.SendKeys(lastName);
+        }
+
+        public void clickBCheckbox()
+        {
+            objCheckBox1.Click();
+        }
+
+        public void clickCCheckbox()
+        {
+            objCheckBox2.Click();
+        }
+
+        public void clickPlusCheckbox()
+        {
+            objCheckBox3.Click();
+        }
+
+        public void clickPCheckbox()
+        {
+            objCheckBox4.Click();
+        }
+
+        public void clickSubmitButton()
+        {
+            objSubmitButton.Click();
+        }
+
+        public void selectDropDownOption1(string option)
+        {
+            var selectElement = new SelectElement(objDropDownOption1);
+            selectElement.SelectByText(option);
+        }
+
+        public void selectDropDownOption2(string option)
+        {
+            var selectElement = new SelectElement(objDropDownOption2);
+            selectElement.SelectByText(option);
+        }
+
+        public string Alert(string message)
+        {
+            var alert = _driver.SwitchTo().Alert();
+            string text = alert.Text;
+            if (!text.Contains(message))
+            {
+                Console.WriteLine("Message not displayed correctly");
+            }
+            else
+            {
+                Console.WriteLine("Message displayed correctly");
+                alert.Accept();
+            }
+
+            return text;
+        }
+
+        public void clearButtons()
+        {
+            if (objCheckBox1.Selected)
+            {
+                objCheckBox1.Click();
+            }
+            if (objCheckBox2.Selected)
+            {
+                objCheckBox2.Click();
+            }
+            if (objCheckBox3.Selected)
+            {
+                objCheckBox3.Click();
+            }
+            if (objCheckBox4.Selected)
+            {
+                objCheckBox4.Click();
+            }
+        }
+
     }
 }
--- challenge-master/Challenge.Test/Web_Tests.cs
+++ challenge-master/Challenge.Test/Web_Tests.cs
@@ -1,14 +1,190 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using BaseFramework.WebPages;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
 
 namespace Challenge.Test
 {
-    [TestClass]
+
     public class Web_Tests
     {
-        [TestMethod]
-        public void MainPage_Correct_Selections_Positive()
+        public IWebDriver driver;
+        public MainPage mainPage = null;
+        public static string url = "http://ztestqa.com/selenium/mainpage.html";
+        public string firstName = "Alejandro";
+        public string lastName = "Perez";
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
         {
+            driver = new ChromeDriver();
+            driver.Url = url;
+            driver.Manage().Window.Maximize();
+            mainPage = new MainPage(driver);
+        }
+
+        [Test]
+        public void MainPage_Positive()
+        {
+            try
+            {
+                string expectedMessage = $"Congratulations {firstName}! Everything was properly populated.";
+                mainPage.clearFirstName();
+                mainPage.clearLastName();
+                mainPage.enterFirstName(firstName);
+                mainPage.enterLastName(lastName);
+                mainPage.clearButtons();
+                mainPage.clickBCheckbox();
+                mainPage.clickPlusCheckbox();
+                mainPage.clickPCheckbox();
+                mainPage.selectDropDownOption1("5");
+                mainPage.selectDropDownOption2("");
+                mainPage.clickSubmitButton();
+                string message = mainPage.Alert(expectedMessage);
+                Assert.AreEqual(expectedMessage, message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        [Test]
+        public void MainPage_Negative_First_Name_Empty()
+        {
+            try
+            {
+                string expectedMessage = "Please enter a first name";
+                mainPage.clearFirstName();
+                mainPage.clearLastName();
+                mainPage.enterLastName(lastName);
+                mainPage.clearButtons();
+                mainPage.clickBCheckbox();
+                mainPage.clickPlusCheckbox();
+                mainPage.clickPCheckbox();
+                mainPage.selectDropDownOption1("5");
+                mainPage.selectDropDownOption2("");
+                mainPage.clickSubmitButton();
+                string message = mainPage.Alert(expectedMessage);
+                Assert.AreEqual(expectedMessage, message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        [Test]
+        public void MainPage_Negative_Last_Name_Empty()
+        {
+            try
+            {
+                string expectedMessage = "Please enter a last name";
+                mainPage.clearFirstName();
+                mainPage.clearLastName();
+                mainPage.enterFirstName(firstName);
+                mainPage.clearButtons();
+                mainPage.clickBCheckbox();
+                mainPage.clickPlusCheckbox();
+                mainPage.clickPCheckbox();
+                mainPage.selectDropDownOption1("5");
+                mainPage.selectDropDownOption2("");
+                mainPage.clickSubmitButton();
+                string message = mainPage.Alert(expectedMessage);
+                Assert.AreEqual(expectedMessage, message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        [Test]
+        public void MainPage_Negative_Wrong_Check_Box_Selected()
+        {
+            try
+            {
+                string expectedMessage = "The checkbox selection is not quite right";
+                mainPage.clearFirstName();
+                mainPage.clearLastName();
+                mainPage.enterFirstName(firstName);
+                mainPage.enterLastName(lastName);
+                mainPage.clearButtons();
+                mainPage.clickBCheckbox();
+                mainPage.clickCCheckbox();
+                mainPage.clickPlusCheckbox();
+                mainPage.clickPCheckbox();
+                mainPage.selectDropDownOption1("5");
+                mainPage.selectDropDownOption2("");
+                mainPage.clickSubmitButton();
+                string message = mainPage.Alert(expectedMessage);
+                Assert.AreEqual(expectedMessage, message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        [Test]
+        public void MainPage_Negative_Wrong_First_Dropdown_Selected()
+        {
+            try
+            {
+                string expectedMessage = "The dropdown selection is not quite right";
+                mainPage.clearFirstName();
+                mainPage.clearLastName();
+                mainPage.enterFirstName(firstName);
+                mainPage.enterLastName(lastName);
+                mainPage.clearButtons();
+                mainPage.clickBCheckbox();
+                mainPage.clickPlusCheckbox();
+                mainPage.clickPCheckbox();
+                mainPage.selectDropDownOption1("4");
+                mainPage.selectDropDownOption2("");
+                mainPage.clickSubmitButton();
+                string message = mainPage.Alert(expectedMessage);
+                Assert.AreEqual(expectedMessage, message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        [Test]
+        public void MainPage_Negative_Wrong_Second_Dropdown_Selected()
+        {
+            try
+            {
+                string expectedMessage = "A selection was made other than the default in select list 2";
+                mainPage.clearFirstName();
+                mainPage.clearLastName();
+                mainPage.enterFirstName(firstName);
+                mainPage.enterLastName(lastName);
+                mainPage.clearButtons();
+                mainPage.clickBCheckbox();
+                mainPage.clickPlusCheckbox();
+                mainPage.clickPCheckbox();
+                mainPage.selectDropDownOption1("5");
+                mainPage.selectDropDownOption2("5");
+                mainPage.clickSubmitButton();
+                string message = mainPage.Alert(expectedMessage);
+                Assert.AreEqual(expectedMessage, message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            driver.Close();
+            driver.Quit();
         }
     }
 }
